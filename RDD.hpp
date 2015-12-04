9bd675b8ece08357832a1be360c563902ce51455
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-12-03 22:39:48 -0700

    Implemented ParallelCollectionRDD, modified MappedRDD and RDD

--- RDD.hpp
+++ RDD.hpp
@@ -1,43 +1,59 @@
-#include <iostream>
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
-template<typename A>
+template<typename T>
+void print_vector(vector<T> v) {
+    for (auto i = v.begin(); i != v.end(); ++i) {
+        cout << *i << ' ' << endl;
+    }
+}
+template<typename A, typename B, typename Func>
 class MappedRDD;
 
 template <typename A>
 class RDD {
 public:
-    vector<A> sequence;
-
-    RDD() {}
-    RDD(vector<A> sequence): sequence(sequence) {}
-
     template<typename Func>
-    auto map(Func f) -> RDD<decltype(f(A()))> {
-        vector<decltype(f(A()))> result;
-        result.resize(sequence.size());
-        std::transform(sequence.begin(), sequence.end(), result.begin(), f);
-        return MappedRDD<decltype(f(A()))>(result);
+    auto map(Func f) -> RDD<decltype(f(A()))>* {
+        return new MappedRDD<A, decltype(f(A())), Func>(this, f);
     }
 
     template<typename Func>
     auto fold(A init, Func f) -> decltype(f(A(), A())) {
+        auto sequence = compute();
         return std::accumulate(std::begin(sequence), std::end(sequence), init, f);
     }
 
-//    virtual vector<A> compute() = 0;
+    virtual vector<A> compute() = 0;
+};
+
+template <typename A, typename B, typename Func>
+class MappedRDD: public RDD<B> {
+public:
+    RDD<A> *prev;
+    Func f;
+    MappedRDD(RDD<A> *prev, Func f): prev(prev), f(f) {}
+    vector<B> compute() {
+        vector<A> prev_vector = prev->compute();
+        vector<decltype(f(A()))> result;
+        result.resize(prev_vector.size());
+        std::transform(prev_vector.begin(), prev_vector.end(), result.begin(), f);
+        return result;
+    }
 };
 
 template <typename A>
-class MappedRDD: public RDD<A> {
+class ParallelCollectionRDD: public RDD<A> {
+    vector<A> sequence;
+
 public:
-    MappedRDD(vector<A> sequence): RDD<A>(sequence) {}
-//    vector<A> compute() {
-//        vector<A> result;
-//        return this->sequence;
-//    }
+    ParallelCollectionRDD(vector<A> sequence): sequence(sequence) {}
+
+    vector<A> compute() {
+        return sequence;
+    }
 };
 
--- main.cpp
+++ main.cpp
@@ -5,30 +5,21 @@
 
 using namespace std;
 
-
-template<typename T>
-void print_vector(vector<T> v) {
-    for (auto i = v.begin(); i != v.end(); ++i) {
-        cout << *i << ' ' << endl;
-    }
-}
-
-
 int main() {
     cout << "Hello, World!" << endl;
     vector<int> data {1, 2, 3, 4};
-    MappedRDD<int> rdd(data);
+    ParallelCollectionRDD<int> rdd(data);
     auto r = rdd.map([](int a) -> double {
         return a * 2;
-    }).map([](double a) -> double {
-        return a + 5;
+    })->map([](double a) -> double {
+        return a * 2;
     });
 
     cout << "Printing numbers\n";
-    print_vector(r.sequence);
+    print_vector(r->compute());
 
     cout << "Printing sum\n";
-    cout << r.fold(0, [](int a, int b) -> int {
+    cout << r->fold(0, [](int a, int b) -> int {
         return a + b;
     });
     return 0;
