691cc249877e52ed32a6b8efe5592967ad7e6c87
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-12-03 18:23:43 -0700

    Try pre declaring class

--- RDD.h
+++ RDD.h
@@ -11,30 +11,30 @@ using std::function;
 #include <vector>
 #include <algorithm>
 
-using namespace std;
-
-template <typename A>
-class RDD {
-public:
-    vector<A> sequence;
-
-    template<typename Func>
-    auto map(Func f) -> RDD<decltype(f(A()))>;
-
-    template<typename Func>
-    auto fold(A init, Func f) -> decltype(f(A(), A()));
-
-    virtual vector<A> compute() = 0;
-};
-
-template <typename A>
-class MappedRDD: public RDD<A> {
-public:
-    MappedRDD(vector<A> sequence);
-
-    vector<A> sequence;
-
-    vector<A> compute();
-};
+//using namespace std;
+//
+//template <typename A>
+//class RDD {
+//public:
+//    vector<A> sequence;
+//
+//    template<typename Func>
+//    auto map(Func f) -> RDD<decltype(f(A()))>;
+//
+//    template<typename Func>
+//    auto fold(A init, Func f) -> decltype(f(A(), A()));
+//
+//    virtual vector<A> compute() = 0;
+//};
+//
+//template <typename A>
+//class MappedRDD: public RDD<A> {
+//public:
+//    MappedRDD(vector<A> sequence);
+//
+//    vector<A> sequence;
+//
+//    vector<A> compute();
+//};
 
 #endif //GRAPPARDD_RDD_H
--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include "RDD.h"
 
 using namespace std;
 
