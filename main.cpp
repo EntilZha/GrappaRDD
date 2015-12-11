94c5beb0a33f94d03f01f976f09a673635f7f8f3
Pedro Rodriguez <ski.rodriguez@gmail.com>
2015-12-10 21:20:18 -0700

    Add basic benchmark

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include "GrappaContext.hpp"
+#include <time.h>
 
 using std::cout;
 
@@ -22,33 +23,41 @@ int add(const int& a, const int& b) {
 int main(int argc, char *argv[]) {
     auto gc = new GrappaContext(argc, argv);
     gc->run([] {
-        vector<int> coll_data(100);
-        for (int i = 0; i < 100; i++) {
-            coll_data[i] = i;
-        }
-        auto collection_rdd = new ParallelCollectionRDD<int>(coll_data);
-        int sum = collection_rdd->fold(0, [](const int& a, const int& b) -> int {return a + b;});
-        cout << "Sum: " << sum << endl;
+        time_t t0;
+        time(&t0);
 
-        cout << "ParallelCollectionRDD\n";
-        vector <int_container> data;
-        for (int i = 0; i < 20; i++) {
-            int_container v;
-            v.a = 2 * i;
-            v.b = i;
-            data.push_back(v);
-        }
+        auto sum = RDD<int64_t>::range(1000000000)->sum();
+        time_t t1;
+        time(&t1);
+        double seconds = difftime(t1, t0);
+        cout << "Time: " << seconds << endl;
+        //vector<int> coll_data(100);
+        //for (int i = 0; i < 100; i++) {
+            //coll_data[i] = i;
+        //}
+        //auto collection_rdd = new ParallelCollectionRDD<int>(coll_data);
+        //int sum = collection_rdd->fold(0, [](const int& a, const int& b) -> int {return a + b;});
+        //cout << "Sum: " << sum << endl;
 
-        auto pcoll = new ParallelCollectionRDD<int_container>(data);
-        auto mpcoll = pcoll->map([](int_container a) -> double_container {
-            double_container v;
-            v.a = 2.5;
-            v.b = 1.5;
-            return v;
-        });
-        for (auto e: mpcoll->collect()) {
-            cout << "a: " << e.a << " b: " << e.b << endl;
-        }
+        //cout << "ParallelCollectionRDD\n";
+        //vector <int_container> data;
+        //for (int i = 0; i < 20; i++) {
+            //int_container v;
+            //v.a = 2 * i;
+            //v.b = i;
+            //data.push_back(v);
+        //}
+
+        //auto pcoll = new ParallelCollectionRDD<int_container>(data);
+        //auto mpcoll = pcoll->map([](int_container a) -> double_container {
+            //double_container v;
+            //v.a = 2.5;
+            //v.b = 1.5;
+            //return v;
+        //});
+        //for (auto e: mpcoll->collect()) {
+            //cout << "a: " << e.a << " b: " << e.b << endl;
+        //}
 
     });
     gc->stop();
